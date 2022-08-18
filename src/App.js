b68e9a8666a4c2b543cdb520f92485551dd4ddd3
Your Name <pankajpandey9589@gmail.com>
2022-08-18 17:52:43 +0530

    created about section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import About from "./components/About/About";
 import Intro from "./components/Introduction/Intro";
 
 function App() {
   return (
     <div className="App">
       <Intro />
+      <About />
     </div>
   );
 }
